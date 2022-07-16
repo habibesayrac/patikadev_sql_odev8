bcae1b03aaf3d270e52e52eb27b82daa702b861f
habibesayrac <90598971+habibesayrac@users.noreply.github.com>
2022-07-16 15:56:47 +0300

    Create sql_odev8.sql

---
+++ sql_odev8.sql
@@ -0,0 +1,25 @@
+
+-- 1 CREATE TABLE employee(
+-- id int,
+-- name VARCHAR(50),
+-- birthday DATE,
+-- email VARCHAR(100)
+--);
+-- 2 insert into employee (id, name, birthday, email) values (1, 'Stephine', '1956-01-04', 'sthatcher0@aboutads.info');
+insert into employee (id, name, birthday, email) values (2, 'Joellen', '1950-11-02', 'jgrundey1@blogs.com');
+insert into employee (id, name, birthday, email) values (3, 'Guenevere', '1969-03-20', 'gfaiers2@google.co.uk');
+insert into employee (id, name, birthday, email) values (4, 'Anya', '1959-12-09', 'acrinson3@multiply.com');
+insert into employee (id, name, birthday, email) values (5, 'Stephani', '1932-05-23', 'sswinney4@berkeley.edu');
+insert into employee (id, name, birthday, email) values (6, 'Rosamund', '1980-07-08', 'rmathis5@arstechnica.com');
+insert into employee (id, name, birthday, email) values (7, 'Glen', '1901-04-30', 'ggooda6@google.com.br');
+insert into employee (id, name, birthday, email) values (8, 'Netty', '1912-08-20', 'ndobel7@harvard.edu');
+insert into employee (id, name, birthday, email) values (9, 'Allistir', '1946-04-08', 'abunhill8@mapquest.com');
+insert into employee (id, name, birthday, email) values (10, 'Jessika', '1958-11-16', 'jshalcros9@about.com');
+insert into employee (id, name, birthday, email) values (11, 'Mary', '1941-12-24', 'mindrucha@lycos.com');
+insert into employee (id, name, birthday, email) values (12, 'Izzy', '1980-02-09', 'idurnob@cnet.com');
+insert into employee (id, name, birthday, email) values (13, 'Herminia', '1973-04-28', 'hthomersonc@amazon.com');
+insert into employee (id, name, birthday, email) values (14, 'Ginny', '1901-10-25', 'gidiensd@google.fr');
+insert into employee (id, name, birthday, email) values (15, 'Connie', '1902-03-08', 'cgerrelse@flickr.com');
+insert into employee (id, name, birthday, email) values (16, 'Craggy', '1948-10-26', 'chartingtonf@blogger.com');
+insert into employee (id, name, birthday, email) values (17, 'Bridgette', '1974-05-04', 'bbasketfieldg@foxnews.com');
+insert into employee (id, name, birthday, email) 
